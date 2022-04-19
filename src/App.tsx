d2872e24867362e4e38b3f1b854400d1741e0a53
Breno Santin <brenolimaa96@gmail.com>
2022-04-19 13:54:55 +0200

    adding module css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, FormEvent, FormEventHandler, useState } from 'react'
 import logo from './logo.svg'
-import './App.css'
+import styles from './App.module.css'
 import { api } from './api';
 
 function App() {
@@ -30,15 +30,15 @@ function App() {
 
   return (
     <div>
-      <div className='loginContainer'>
+      <div className={styles.loginContainer}>
         <div>
-          <h1 className='text-5xl'>Login</h1>
+          <h1 className={styles.titleH1}>Login</h1>
         </div>
         <div>
-          <form className='loginForm'>
-            <input id='emailInput' placeholder='Informe seu email' onChange={emailInputValue} className='placeholder-gray-500 placeholder-opacity-50' type="text" />
-            <input id='passInput' placeholder='Informe sua senha' onChange={passwordInputValue} className='placeholder-gray-500 placeholder-opacity-50' type="password" />
-            <button type='submit' className='buttonLogin' onClick={handleLoginButton}>Entrar</button>
+          <form className={styles.loginForm}>
+            <input id='emailInput' placeholder='Informe seu email' onChange={emailInputValue} type="text" />
+            <input id='passInput' placeholder='Informe sua senha' onChange={passwordInputValue} type="password" />
+            <button type='submit' className={styles.buttonLogin} onClick={handleLoginButton}>Entrar</button>
           </form>
         </div>
       </div>
