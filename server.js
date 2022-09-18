61e794032e7f1a6d0e67cad01509afee34690c9a
Breno Santin <brenolimaa96@gmail.com>
2022-09-18 07:36:40 -0300

    kda

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("Batata");
+const express = require('express');
+const dot = require('dotenv');
+const path = require('path')
+const app = express();
+dot.config();
+
+
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.listen(process.env.PORT || 3000, (err) => {
+    if (err) {
+        console.log("ERRO: ", err)
+    } else {
+        console.log("Tudo funcionando.")
+    }
+})
