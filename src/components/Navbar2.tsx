8f955d0ed9b8996c3e20a896ceb0700b87919337
Breno Santin <brenolimaa96@gmail.com>
2022-11-03 14:38:44 -0300

    fixed NavbarBug

--- src/components/Navbar2.tsx
+++ src/components/Navbar2.tsx
@@ -15,7 +15,7 @@ export const Navbar2 = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className="collapse navbar-collapse collapsedNavBar" id="navbarNav">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item p-2">
                             <NavLink className="linksnavbarMenu" to="/main">Home</NavLink>
